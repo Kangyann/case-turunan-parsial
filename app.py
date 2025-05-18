2e21709fcc6d20a8f2b235a9eb84bc6f64444293
Dian <113026132+Kangyann@users.noreply.github.com>
2025-05-18 15:02:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 
 # Set judul halaman
 st.set_page_config(page_title="Simulasi Produksi Gasket & Reinf", layout="centered")
-st.title("📦 Simulasi Biaya Produksi Harian (Gasket & Reinf)")
-st.title("Kelompok 6" )
+st.title("📦 Simulasi Biaya Produksi Harian (Gasket & Reinf) --")
 
 # Konstanta produksi
 lot_gasket = 3600
