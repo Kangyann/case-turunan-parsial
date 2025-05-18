a7350470c19467d741286e26b14595e712f093d9
Dian <113026132+Kangyann@users.noreply.github.com>
2025-05-18 18:27:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,8 @@ st.dataframe(df.style.format({
 
 # Line chart
 st.subheader("📈 Grafik Line Chart: Total Biaya Produksi")
-st.line_chart(df.set_index("Hari")[["Total Biaya"]])
-st.line_chart(df.set_index("Hari")[["Total Reinf"]])
-st.line_chart(df.set_index("Hari")[["Total Gasket"]])
+st.line_chart(df.set_index("Hari")[["Total Biaya", "Total Reinf", "Biaya Gasket"]])
+
 
 # Pyplot chart
 st.subheader("📉 Grafik Pyplot: Total Biaya Produksi")
